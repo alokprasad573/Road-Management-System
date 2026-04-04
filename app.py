890a501e5be617619379dd36e76efbd20cafc68e
Alok Prasad <alokprasad573@outlook.com>
2026-04-04 20:35:17 +0530

    app.py modified.

--- app.py
+++ app.py
@@ -486,6 +486,6 @@ def register_routes(app: Flask) -> None:
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=Config.FLASK_PORT, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
